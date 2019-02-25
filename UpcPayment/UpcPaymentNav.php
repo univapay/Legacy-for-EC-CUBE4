4be2703de63376c26694719a3094777f7cafade2
yamagata <k.yamagata@univapay.com>
2019-02-25 10:22:11 +0900

    First commit
    First commit

---
+++ UpcPayment/UpcPaymentNav.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Plugin\UpcPayment;
+
+use Eccube\Common\EccubeNav;
+
+class UpcPaymentNav implements EccubeNav
+{
+    /**
+     * @return array
+     */
+    public static function getNav()
+    {
+        return [
+            'order' => [
+                'children' => [
+                    'upc_payment_admin_payment_status' => [
+                        'name' => 'upc_payment.admin.nav.payment_list',
+                        'url' => 'upc_payment_admin_payment_status',
+                    ],
+                ],
+            ],
+        ];
+    }
+}
